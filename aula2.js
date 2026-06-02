85911154b35219abdc00c3c8bf056ee4728d43aa
victo <victoriagabriellecamargo@gmail.com>
2026-06-01 23:18:04 -0300

    feat: add initial variables and log calculations

---
+++ aula2.js
@@ -0,0 +1,9 @@
+let minhavariavel = 20;
+let outravariavel = 15;
+let soma = minhavariavel + outravariavel;
+console.log(soma);
+
+console.log(10 + minhavariavel);
+
+let segundasoma = minhavariavel + outravariavel + 10;
+console.log(segundasoma);
