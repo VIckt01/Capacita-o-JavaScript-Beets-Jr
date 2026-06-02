91238ed61bea951006d89e90bbd6b559f939813e
victo <victoriagabriellecamargo@gmail.com>
2026-06-01 23:14:30 -0300

    refactor: reorganize console log statements and variable assignments

--- aula1.js
+++ aula1.js
@@ -1,3 +1,16 @@
 let minhavariavel = 20;
 let minhaString = "Hello Word";
+
+console.log(minhaString);
+
+minhaString = "Curso de React";
+
+console.log(minhaString);
+
+minhavariavel = 50;
+
+console.log(minhavariavel);
+
+minhaString = 20;
+
 console.log(minhaString);
