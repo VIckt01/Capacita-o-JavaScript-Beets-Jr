b9ca666ab7f2b25df8c3b521fb1710abe0a071e0
victo <victoriagabriellecamargo@gmail.com>
2026-06-01 23:37:42 -0300

    fix: uncomment salas variable and add division calculation

--- aula2.js
+++ aula2.js
@@ -7,7 +7,7 @@ let segundasoma = minhavariavel + outravariavel + 10;
 console.log(segundasoma);
 
 let quantidadeAlunosSala = 15;
-//let salas = 10;
+let salas = 10;
 let quantidadeTotal = quantidadeAlunosSala * salas;
 
 //multiplicação
@@ -15,3 +15,10 @@ console.log(quantidadeTotal);
 
 console.log(2 * 7 * 2);
 
+//divisão
+let numeroComVirgula = 12.15;
+let numeroA = 10;
+let numeroB = 2;
+let divisao = numeroA / numeroB;
+
+console.log(divisao);
