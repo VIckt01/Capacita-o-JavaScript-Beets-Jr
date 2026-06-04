8dfad7de9de0f85d465ee50b2b2956364ead67d2
victo <victoriagabriellecamargo@gmail.com>
2026-06-04 20:22:21 -0300

    feat: add aula5.js with array manipulations and console logs

---
+++ js/aula5.js
@@ -0,0 +1,20 @@
+let lista = ['Ana', 'Beatriz', 'José', 'Mateus', 'André'];
+let listaDeNumeros = [22, 35, 70];
+
+console.log(listaDeNumeros[0]);
+console.log(listaDeNumeros[0] + listaDeNumeros[2]);
+
+listaDeNumeros.push(40);
+console.log(listaDeNumeros);
+
+lista.push('Marcos');
+console.log(lista);
+
+lista.pop();
+console.log(lista);
+
+lista.reverse();
+console.log(lista);
+
+lista[0] = 'Leonardo';
+console.log(lista);
